7bc0bfcf8a8f3d6f6cdbc54a37eb39ae88770840
jasongreen <jasongreen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-18 22:14:52 +0000

    First release
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-submissions/trunk@1288985 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ contact-form-submissions.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Contact Form Submissions
+Description: Save all Contact Form 7 submissions in the database.
+Version:     1.0
+Author:      Jason Green
+License:     GPLv3
+*/
+
+define('WPCF7S_TEXT_DOMAIN', 'wpcf7-submissions');
+define('WPCF7S_DIR', realpath(dirname(__FILE__)));
+define('WPCF7S_FILE', 'contact-form-7-submissions/contact-form-7-submissions.php');
+
+require_once WPCF7S_DIR . '/Submissions.php';
+require_once WPCF7S_DIR . '/Admin.php';
+
+function contact_form_7_submissions_init() {
+  global $contact_form_7_submissions;
+  $contact_form_7_submissions = new WPCF7Submissions();
+}
+add_action( 'init', 'contact_form_7_submissions_init', 9 );
+
+function contact_form_7_submissions_admin_init() {
+  global $contact_form_7_submissions_admin;
+  $contact_form_7_submissions_admin = new WPCF7SAdmin();
+}
+add_action( 'admin_init', 'contact_form_7_submissions_admin_init' );
